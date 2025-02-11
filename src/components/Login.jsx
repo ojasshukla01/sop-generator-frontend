6d30f0a138a50da73616db18c07fd855a03d27b5
Ojas Shukla <ojasshukla01@gmail.com>
2025-02-11 11:39:10 +1100

    Update Login component to use x-www-form-urlencoded for authentication

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,7 +11,18 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:8000/token", formData);
+      const response = await axios.post(
+        "http://localhost:8000/token",
+        new URLSearchParams({
+          username: formData.username,
+          password: formData.password,
+        }),
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
       alert("Login successful!");
       console.log(response.data);
     } catch (error) {
